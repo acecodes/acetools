ebde10d49b4118d59bdf6d959aec3392e11a55eb
Ace <ace.github@gmail.com>
2014-11-27 22:51:47 -0800

    Converted serializer to ModelSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,7 @@ from django.forms import widgets
 from rest_framework import serializers
 from users.models import User
 
-class UserSerializer(serializers.Serializer):
-	first_name = serializers.CharField(required=True, max_length=50)
-	last_name = serializers.CharField(required=True, max_length=50)
-	email = serializers.EmailField(required=True, max_length=50)
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('id', 'title', 'code', 'lineos', 'language', 'style')
