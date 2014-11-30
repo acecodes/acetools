80aabf482406af6cf6e8faba02e1190e34e28c5d
Ace <ace.github@gmail.com>
2014-11-29 22:47:26 -0800

    Started on URLs and views

--- acetools/urls.py
+++ acetools/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from skillstreak.views import index
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^', include('tools.urls')),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', index),
 )
